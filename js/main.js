74dcb10a6398df74137724c19ab8e401a23534ac
faria <faria@DESKTOP-TLUUL68>
2023-02-23 22:44:58 -0300

    terminando los filtros events

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* let htmlEvent= "";
+
+for (const event of data.events) {
+    htmlEvent += `<div class="col">
+    <div class="card">
+      <img src="${event.image}" class="rounded float-start card-img-top" alt="Maraton">
+      <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p>${event.description}</p>
+        <p>Precio $${event.price}</p><button type="button" class="btn     btn-primary">More...</button>
+      </div>
+    </div>
+    </div> `
+
+}
+console.log(htmlEvent); */
+
+
---
+++ js/pastEvent.js
@@ -0,0 +1,23 @@
+let htmlPastEvent =" ";
+console.log("-----------------------"); 
+console.log("-----------------------"); 
+console.log("Eventos Pasados"); 
+
+for (const event of data.events) {
+ let currentDate = new Date(data.currentDate);
+let date = new Date(event.date);
+if(currentDate > date){    
+    htmlPastEvent += `<div class="col">
+    <div class="card">
+    <img src="${event.image}" class="rounded float-start card-img-top" alt="Maraton">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5>
+    <p>${event.description}</p>
+    <p>Precio $${event.price}</p><button type="button" class="btn     btn-primary">More...</button>
+    </div>
+    </div>
+    </div> `
+   
+}
+}
+console.log(htmlPastEvent); 
